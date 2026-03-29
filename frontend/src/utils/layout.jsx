52ab2df191d6b3a64a76a273915a192bf448eea7
MD. RAKIBUL HASAN HEMEL <rhn109820@gmail.com>
2026-03-29 13:52:30 +0600

    Scroll Behavior fixed

--- frontend/src/utils/layout.jsx
+++ frontend/src/utils/layout.jsx
@@ -4,10 +4,12 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import WhatsAppButton from "../components/Common/WhatsAppButton";
 import BottomNav from "../components/BottomNav";
+import ScrollToTop from "../components/ScrollToTop";
 
 function Layout() {
   return (
     <>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen relative">
         <Navigation />
         <main className="grow relative z-30">
